1b25f98063a8d15fbbffe355540f86e8d4509861
Rosie Allison-Corio <rosievespa@gmail.com>
2025-01-13 13:54:43 +0000

    workshop done

---
+++ app.js
@@ -0,0 +1,45 @@
+console.log('Hello!')
+
+//creating array
+
+const blogPosts = [
+    "My first blog post",
+    "My second blog post",
+    "My third blog post",
+]
+
+console.log(blogPosts)
+
+//accessing array items
+
+console.log(blogPosts[0]) // my first..
+console.log(blogPosts[1]) // my second..
+console.log(blogPosts[2]) // my third..
+
+//adding items
+
+blogPosts[3] = "My fourth blog post";
+
+console.log(blogPosts[3])
+
+const favouriteFoods = [
+    "Lasagne",
+    "Bolognese",
+    "Celery",
+]
+
+const favouriteColours = [
+    "Green",
+    "Purple",
+    "Orange",
+]
+
+const favouriteNumbers = [
+    54,
+    7,
+    16,
+]
+
+console.log(favouriteFoods)
+console.log(favouriteColours)
+console.log(favouriteNumbers)
